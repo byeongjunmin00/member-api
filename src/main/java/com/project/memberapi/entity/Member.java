b798c999b0a77b76773526fbe4ce5a76a1ce74d4
민병준 <minbyeongjun@minbyeongjun-ui-MacBookAir.local>
2026-05-20 21:08:54 +0900

    프로필 이미지 필드 추가 + S3 SDK 의존성 추가

--- src/main/java/com/project/memberapi/entity/Member.java
+++ src/main/java/com/project/memberapi/entity/Member.java
@@ -22,9 +22,15 @@ public class Member {
     @Column(nullable = false)
     private String mbti;
 
+    private String profileImageUrl;
+
     public Member(String name, int age, String mbti) {
         this.name = name;
         this.age = age;
         this.mbti = mbti;
+    }
+
+    public void updateProfileImageUrl(String profileImageUrl) {
+        this.profileImageUrl = profileImageUrl;
     }
 }
